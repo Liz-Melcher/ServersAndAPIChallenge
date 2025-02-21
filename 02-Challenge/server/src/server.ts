4f7aa8fe2d1dfb546e2d4802d2b855f88a11afe1
Liz Melcher <lizmelchercodingbootcamp@gmail.com>
2025-02-21 16:22:51 -0600

    updated ts config, updated server.ts file

--- 02-Challenge/server/src/server.ts
+++ 02-Challenge/server/src/server.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path'; //Liz added this 2/21/2025 
+import { fileURLToPath } from 'url'; // Liz added this
 dotenv.config();
 
 // Import the routes
@@ -10,11 +12,17 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}));
 
 // TODO: Implement middleware to connect the routes
 app.use(routes);
 
-// Start the server on the port
+// Start the server on the ports
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
