6e55984fb5b5d04e332a455b3380e9b7b53f80cf
Liz Melcher <lizmelchercodingbootcamp@gmail.com>
2025-02-24 12:29:20 -0600

    updating weatherRoutes

--- 02-Challenge/server/src/routes/api/weatherRoutes.ts
+++ 02-Challenge/server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,55 @@
 import { Router, type Request, type Response } from 'express';
+import historyService from '../../service/historyService';
+import weatherService from '../../service/weatherService';
+
 const router = Router();
 
 // import HistoryService from '../../service/historyService.js';
 // import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const {city} = req.body;
+    if (!city) {
+      return res.status(400).json({error: 'City is required'});
+    }
+    const weatherData = await weatherService.getWeatherForCity(city);
+    await historyService.addCity(city);
+    res.status(200).json(weatherData);
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).json({error: error.message || 'Internal Server Error'});
+  }
+
+
+
   // TODO: GET weather data from city name
   // TODO: save city to search history
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (req: Request, res: Response) => {
+  try {
+    const history = await historyService.getCities();
+    return res.status(200).json(history);
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).json({ error: error.message || 'Internal Server Error'})
+  }
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const updateHistory = await historyService.removeCity(id);
+    res.status(200).json(updateHistory);
+  } catch (error: any) {
+    console.error(error); 
+    res.status(500).json({ error: error.message || 'Internal Server Error'});
+  }
+  }
+);
 
 export default router;
