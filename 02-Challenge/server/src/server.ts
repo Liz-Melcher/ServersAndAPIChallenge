b08b399795da9c797f8d1bce646138b5d3ef4153
Liz Melcher <lizmelchercodingbootcamp@gmail.com>
2025-02-27 19:14:28 -0500

    updated index html

--- 02-Challenge/server/src/server.ts
+++ 02-Challenge/server/src/server.ts
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT: number = parseInt(process.env.PORT as string, 10) || 3001;
+const PORT = process.env.PORT || 3001;
 
 
 //console.log('Loaded API Key:', process.env.API_KEY);
